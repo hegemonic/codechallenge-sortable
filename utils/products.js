034a170c997523a9353cd0f39a3ec3bef7e95683
afrolionx <mike@mikecorp.ca>
2016-08-16 18:48:53 -0400

    removed superfluous code

--- utils/products.js
+++ utils/products.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const Async = require('async');
 const Path = require('path');
-const _ = require('lodash');
-
-const Manufacturer = require('../libs/manufacturer');
 
 /**
  * Returns a file path to the products data
@@ -12,35 +8,4 @@ const Manufacturer = require('../libs/manufacturer');
  */
 exports.getFilePath = function getFilePath () {
   return Path.resolve(__dirname, '..', 'products.txt');
-};
-
-exports.determineManufacturers = function determineManufacturers (products) {
-  let output = [];
-  Async.each(products, (product, doneProduct) => {
-    let productName = product.manufacturer.toLowerCase();
-    Async.each(output, (item, doneOutput) => {
-      if (item.name === productName) {
-        doneOutput(item);
-      } else {
-        doneOutput(null);
-      }
-    }, (foundMatch) => {
-      if (foundMatch) {
-        foundMatch.increment();
-      } else {
-        let tempManu = new Manufacturer(productName);
-        if (productName.split(' ').length > 1) {
-          tempManu.isMultiWord();
-        }
-        output.push(tempManu);
-      }
-    });
-    doneProduct(null);
-  }, (err) => {
-    if (err) console.log(err);
-    output = _.chain(output)
-      .orderBy(s => s.count, 'desc')
-      .value();
-  });
-  return output;
 };
