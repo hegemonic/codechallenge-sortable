7ca15498028f5bf0bb66513335767f3ef95160bb
afrolionx <mike@mikecorp.ca>
2016-08-17 14:31:52 -0400

    added documentation to manufacturer class

--- libs/manufacturer.js
+++ libs/manufacturer.js
@@ -1,16 +1,28 @@
 'use strict';
 
+/**
+ * Class representing a manufacturer
+ */
 module.exports = class Manufacturer {
+  /**
+   * Create a manufacturer
+   * @param {String} name The name of the manufacturer
+   */
   constructor (name) {
     this.name = name;
     this.multiWord = false;
     this.listings = [];
   }
-
+  /**
+   * Setter function for if this manufacturer has more than one word in the name
+   */
   isMultiWord () {
     this.multiWord = true;
   }
-
+  /**
+   * Getter function for the name of this manufacturer
+   * @returns {String}  The lowercase name of this manufacturer
+   */
   getName () {
     return this.name.toLowerCase();
   }
