f4100e16fa1bdbe2e3daf6c5e0661a49d2f9b100
afrolionx <mike@mikecorp.ca>
2016-08-17 16:54:49 -0400

    Added documentation to the term class

--- libs/term.js
+++ libs/term.js
@@ -1,6 +1,14 @@
 'use strict';
 
+/**
+ * Class representation of a term
+ */
 module.exports = class Term {
+  /**
+   * Create a term
+   * @param {String} name The name of the term, which is just the string text
+   *                      of the term.
+   */
   constructor (name) {
     this.name = name;
     this.lex = '';
